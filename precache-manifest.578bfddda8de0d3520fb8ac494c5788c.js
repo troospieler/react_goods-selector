def9f2aab8a6dacd1ee206d755bff0e5a009e0b1
troospieler <troospieler1990@gmail.com>
2021-01-18 13:15:37 +0200

    Updates

---
+++ precache-manifest.578bfddda8de0d3520fb8ac494c5788c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61e48aac08a6a7822e45ee9e3a112ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ea67adfcfcf8d837d0c",
+    "url": "./static/css/main.aaab082f.chunk.css"
+  },
+  {
+    "revision": "b5ebe3227a36fe6a4ae7",
+    "url": "./static/js/2.c3d77166.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c3d77166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea67adfcfcf8d837d0c",
+    "url": "./static/js/main.5ab56977.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
